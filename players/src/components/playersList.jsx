8b3cc4fb5269cd373825db50bbbd64e2b716d35c
deepesh06-95 <91532309+deepesh06-95@users.noreply.github.com>
2022-03-24 11:59:12 +0530

    done except search funtionality and styling

---
+++ players/src/components/playersList.jsx
@@ -0,0 +1,63 @@
+import react from 'react';
+import "./playersList.css"
+import { useState,useEffect } from 'react';
+function importAll(r) {
+    let images = {};
+    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+    return images;
+  }
+  
+  const images = importAll(require.context('./playerImages', false, /\.(png|jpe?g|svg)$/));
+
+export const List=()=>{
+    const[formData,setFormData]=useState([])
+
+    const getplayer=async()=>{
+      const response=await fetch("https://api.npoint.io/20c1afef1661881ddc9c");
+      const ex=await response.json();
+      setFormData(ex.playerList.sort((a,b)=>a.Value-b.Value));
+    }
+    useEffect(()=>{
+        getplayer();
+        console.log(formData)
+    },[])
+ function display(time){
+  var date = new Date(`${time} UTC`);
+  date.toString();
+  return date.toTimeString();
+ }
+  return (
+      
+      <>
+      <input placeholder='Search player' value="player"></input>
+      <button onClick={search}>Search</button>
+      {formData.map((e,i)=>{
+          return (
+          <div key={i} className="box">
+              <img src={images[`${e.Id}.jpg`]} alt=" Sorry! image not available"/>
+             <div>
+             <h2>Player's Name {e.PFName}</h2>
+              
+              <h4>Skills {e.SkillDesc}</h4>
+             
+              <h4>Value - ${e.Value}</h4>
+              <h4>Upcoming Matches - </h4>
+              <div>
+                {e.UpComingMatchesList.map((mat,i)=>(
+                  <div key={i}>
+                  <h4>Match {i+1}</h4>
+                  <h4>{mat.CCode} vs {mat.VsCCode}</h4>
+                  <h4>Match Date - {display(mat.MDate)}</h4>
+                  </div>
+                ))}
+              </div>
+              
+             </div>
+              
+          </div>)
+      })}
+     
+        
+      </>
+  )  
+}
